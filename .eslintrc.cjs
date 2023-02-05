1396aeeb19bdb8bf75adb400ed27f570b706f0c2
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-02-05 08:44:58 +0100

    chore: update

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: '@damilaredev/eslint-config-typescript',
+  plugins: ['prettier'],
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: ['path'],
+      },
+    ],
+    'import/no-named-as-default': 'off',
+    quotes: [
+      0,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-useless-constructor': 'warn',
+    'space-before-function-paren': 'off',
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+      },
+    },
+  ],
+}
